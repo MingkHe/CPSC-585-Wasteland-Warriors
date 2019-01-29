e550808bddbd3565680fe19c4d3220cab9bbdd11
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2019-01-29 08:13:56 -0700

    Add files via upload

--- Wasteland_Warrior/shaders/vertex.glsl
+++ Wasteland_Warrior/shaders/vertex.glsl
@@ -8,8 +8,10 @@
 
 // location indices for these attributes correspond to those specified in the
 // InitializeGeometry() function of the main program
-layout(location = 0) in vec2 VertexPosition;
+layout(location = 0) in vec3 VertexPosition;
 layout(location = 1) in vec3 VertexColour;
+layout(location = 2) in vec3 Normal;
+layout(location = 3) in unsigned int Index;
 
 // output to be interpolated between vertices and passed to the fragment stage
 out vec3 Colour;
@@ -17,7 +19,7 @@ out vec3 Colour;
 void main()
 {
     // assign vertex position without modification
-    gl_Position = vec4(VertexPosition, 0.0, 1.0);
+    gl_Position = vec4(VertexPosition, 1.0);
 
     // assign output colour to be interpolated
     Colour = VertexColour;

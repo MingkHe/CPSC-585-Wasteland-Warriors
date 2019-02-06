de510f36ab2d126a8bc47beb629495ed18cf26cf
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2019-02-06 16:58:47 -0700

    Add files via upload

--- Wasteland_Warrior/Wasteland_Warrior/Gamestate.h
+++ Wasteland_Warrior/Wasteland_Warrior/Gamestate.h
@@ -13,8 +13,11 @@ public:
 
 	double time;
 	double timeStep;
+
+	//graphics shit
 	Camera camera;
 	glm::vec3 light = glm::vec3(2.f, 2.f, 0.f);
+	unsigned char shading_model = 0;
 
 	std::string state;
 	std::list<Entity> Entities;

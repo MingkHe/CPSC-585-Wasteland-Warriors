6c09f7717288cab063ae45d07de84b54b9091fc3
Matthew Hylton <wehttamh11@gmail.com>
2019-02-28 09:47:36 -0700

    Gamestate logic

--- Wasteland_Warrior/Wasteland_Warrior/Gamestate.cpp
+++ Wasteland_Warrior/Wasteland_Warrior/Gamestate.cpp
@@ -11,38 +11,82 @@ Gamestate::~Gamestate()
 
 void Gamestate::SpawnEnemy(int type, float x, float y) {
 
+	//Add to entity system
 	Enemies.push_back(EnemyUnit(type, x, y));
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::DespawnEnemy(EnemyUnit enemy) {
 
+	//Add to entity system
 	//Enemies.remove(enemy);
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::SpawnPowerUp(int type, float x, float y) {
 
+	//Add to entity system
 	PowerUps.push_back(PowerUp(type, x, y));
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::DespawnPowerUp(PowerUp powerUp) {
 
+	//Add to entity system
 	//PowerUps.remove(powerUp);
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::SpawnObject(int type, float x, float y) {
 
+	//Add to entity system
 	if (type == 0) {
 		DynamicObjects.push_back(Object(type, x, y));
 	}
 	else {
 		StaticObjects.push_back(Object(type, x, y));
 	}
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::DespawnObject(Object object) {
 
+	//Add to entity system
 	//Objects.remove(object);
+
+	//Add to physics system
+
+	//Add to graphics system
+
+	//Mesh/Textures?
 }
 
 void Gamestate::Collision(Entity entity1, Entity entity2, float speed1, float speed2) {
+	//Health update
+	//Sound effect
 }

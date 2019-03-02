1bbf596f20b166adb3dea54ec45d2c322d4574f6
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2019-03-02 16:23:02 -0700

    Add files via upload

---
+++ Wasteland_Warrior/shaders/radarfragment.glsl
@@ -0,0 +1,34 @@
+#version 410
+#define xmargin 0.1f
+#define ymargin 0.1f
+
+// interpolated colour received from vertex stage
+in vec2 position;
+
+uniform vec2 enemies[50];
+uniform int numenemies;
+uniform vec2 playerpos;
+
+// first output is mapped to the framebuffer's colour index by default
+out vec4 FragmentColour;
+
+float radius_squared(vec2 point1, vec2 point2) {
+	vec2 tmp = (point1-point2)*(point1-point2);
+	return tmp.x + tmp.y;
+}
+
+void main(void) {
+    // write colour output without modification
+	FragmentColour = vec4(.3, .3, .3, 0);
+	for(int i = 0; i < numenemies; i++) {
+		if(radius_squared(enemies[i]-playerpos, position) < 0.005) {
+			FragmentColour = vec4(.9, 0, 0, 0);
+		}
+	}
+	if(radius_squared(vec2(0), position) < 0.005) {
+		FragmentColour = vec4(0, .9, 0, 0);
+	}
+	if(position.x > 1-xmargin || position.x < xmargin-1 || position.y > 1-ymargin || position.y < ymargin-1) {
+		FragmentColour = vec4(0, 0, 0, 0);
+	}
+}

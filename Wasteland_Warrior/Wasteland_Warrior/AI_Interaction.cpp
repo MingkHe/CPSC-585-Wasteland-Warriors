6871497631f80e8257b40f717d7a9d61bd0108c8
Christopher Smith <smithcg1@ucalgary.ca>
2019-03-04 04:58:25 -0700

    Merge resolution

--- Wasteland_Warrior/Wasteland_Warrior/AI_Interaction.cpp
+++ Wasteland_Warrior/Wasteland_Warrior/AI_Interaction.cpp
@@ -29,17 +29,32 @@ int AI_Interaction::Update()
 		glm::vec2 enemyHeading = glm::normalize(enemy.direction);
 		glm::vec2 enemyPosition = {enemy.position.x, enemy.position.z};
 
-		//std::cout << "I am at: [" << enemyPosition.x << "," << enemyPosition.y << "] and I would like to get to: [" << targetPosition.x << "," << targetPosition.y << "]" << std::endl;
-
+		std::cout << "I am at: [" << enemyPosition.x << "," << enemyPosition.y << "] and I would like to get to: [" << targetPosition.x << "," << targetPosition.y << "]" << std::endl;
 
 		float enemyRotation = glm::atan(enemyHeading.y / enemyHeading.x);
+		if (enemyHeading.x < 0 && enemyRotation > 0)
+			enemyRotation -= M_PI;
 
-		//std::cout << "enemyRotation in rad: " << enemyRotation << std::endl;
+		if (enemyHeading.x < 0 && enemyRotation < 0)
+			enemyRotation += M_PI;
+
+
+
+		std::cout << "enemyHeading: [" << enemyHeading.x << "," << enemyHeading.y << "]" << std::endl;
+		std::cout << "enemyRotation in rad: " << enemyRotation << std::endl;
 
 		glm::vec2 targetVector = normalize(targetPosition - enemyPosition);
 		float targetRotation = glm::atan(targetVector.y / targetVector.x);
+		if (targetVector.x < 0 && targetRotation > 0)
+			targetRotation -= M_PI;
 
-		//std::cout << "targetRotation in rad: " << targetRotation << std::endl;
+		if (targetVector.x < 0 && targetRotation < 0)
+			targetRotation += M_PI;
+
+
+
+		std::cout << "targetVector: [" << targetVector.x << "," << targetVector.y << "]" << std::endl;
+		std::cout << "targetRotation in rad: " << targetRotation << std::endl;
 
 		float relativeRotation;
 		glm::vec2 controllInput;		//{(Speed -1 -> 1), (Turning -1 (left) -> 1 (right))} 
@@ -47,8 +62,10 @@ int AI_Interaction::Update()
 			controllInput = {1.0f, 0.0f};			//Dont turn
 		}
 		else {  //target - enemy   or 
-			relativeRotation = targetRotation -enemyRotation;
-			//std::cout << "relativeRotation is: " << relativeRotation << std::endl;
+			relativeRotation = targetRotation - enemyRotation;
+
+			std::cout << "relativeRotation is: " << relativeRotation << std::endl;
+
 			if (relativeRotation > (M_PI))
 				relativeRotation -= (2 * M_PI);
 

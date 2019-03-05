059c323c3a2485301e574d3760af583950cb3258
Christopher <smithcg@ucalgary.ca>
2019-03-05 12:10:01 -0700

    Removed prints

--- Wasteland_Warrior/Wasteland_Warrior/EnemyUnit.cpp
+++ Wasteland_Warrior/Wasteland_Warrior/EnemyUnit.cpp
@@ -26,22 +26,22 @@ bool EnemyUnit::CheckForStuck(){
 	ftime(&currentTime);
 	double elapsed_seconds = currentTime.time - lastMotionTime;
 
-	std::cout << "Current time: " << currentTime.time << "     and last movement was at: " << lastMotionTime << std::endl;
-	std::cout << "Elapsed stuck seconds = " << elapsed_seconds << std::endl;
+	//std::cout << "Current time: " << currentTime.time << "     and last movement was at: " << lastMotionTime << std::endl;
+	//std::cout << "Elapsed stuck seconds = " << elapsed_seconds << std::endl;
 
 	
 	
 	if (recoveryMode == false) {			//Car is not currently marked as stuck
-		std::cout << "Recovery mode OFF" << std::endl;
+		//std::cout << "Recovery mode OFF" << std::endl;
 		if (speed > 1) {				//Check if car is moving
 			lastMotionTime = currentTime.time;
-			std::cout << "lastMotionTime updated to: " << lastMotionTime << std::endl;
+			//std::cout << "lastMotionTime updated to: " << lastMotionTime << std::endl;
 			return false;
 		}
 
 		if (elapsed_seconds >= 2) {	//Car is moving so check if stuck for more than 2 seconds
 			recoveryMode = true;
-			std::cout << "Recovery mode turned ON" << std::endl;
+			//std::cout << "Recovery mode turned ON" << std::endl;
 			return true;
 		}
 
@@ -49,12 +49,12 @@ bool EnemyUnit::CheckForStuck(){
 	}
 
 	else {
-		std::cout << "Recovery mode ON" << std::endl;
+		//std::cout << "Recovery mode ON" << std::endl;
 		if (elapsed_seconds >= 4) {	//For 2 more seconds remain in recovery mode
 			recoveryMode = false;
-			std::cout << "Recovery mode turned OFF" << std::endl;
+			//std::cout << "Recovery mode turned OFF" << std::endl;
 			lastMotionTime = currentTime.time;
-			std::cout << "lastMotionTime updated to: " << lastMotionTime << std::endl;
+			//std::cout << "lastMotionTime updated to: " << lastMotionTime << std::endl;
 			return false;
 		}
 

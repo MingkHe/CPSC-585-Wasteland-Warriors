c15cb940f4ab352b4dee89fe622c9d8676083804
Matthew Hylton <wehttamh11@gmail.com>
2019-03-08 14:04:56 -0700

    Logic code has been made much more stable

--- Wasteland_Warrior/Wasteland_Warrior/Logic.cpp
+++ Wasteland_Warrior/Wasteland_Warrior/Logic.cpp
@@ -26,17 +26,13 @@ void Logic::Update(Gamestate *gameState)
 		gameState->restart = false;
 
 		//Reset Car
-		gameState->physics_Controller->setPosition(gameState->playerVehicle.physicsIndex, glm::vec3{ 0, 0, 0 });
+		gameState->physics_Controller->setPosition(gameState->playerVehicle.physicsIndex, glm::vec3{ 0, 2, 0 });
 		gameState->playerVehicle.health = 100;
-
 
 		//Despawn Enemies ***
 		//reset enemies
-		if (gameState->Enemies.size() > 1) {
-			gameState->Enemies.erase(gameState->Enemies.begin(), gameState->Enemies.begin() + gameState->Enemies.size() - 1);
-		}
 		for (int i = 0; i < gameState->Enemies.size(); i++) {
-			gameState->physics_Controller->setPosition(gameState->Enemies[i].physicsIndex, glm::vec3{ 10000, 10000, 10000 });
+			gameState->physics_Controller->setPosition(gameState->Enemies[i].physicsIndex, glm::vec3{ 10000 + (i * 10), 10000 + (i * 10), 10000 + (i * 10) });
 		}
 	}
 
@@ -51,58 +47,29 @@ void Logic::Update(Gamestate *gameState)
 
 		//Setup
 		if (gameState->wave == 0) {
-
-			//Start wave 1
 			gameState->wave = 1;
 			waveBreak = 1;
 			breakTime = 1800;
-
-			modeSelection(gameState, gameState->wave);
-			//gameState->SpawnEnemy(0, 35 + (0 * 10), 5, 35 + (0 * 10));
-			//gameState->Enemies[1].health = 75;
+			modeSelection(gameState);
 		}
 
 
 		//WAVE 1
 		else if (gameState->wave == 1) {
-
-			//Check Enemy health
-			enemiesLeft = 0;
-			for (int i = 1; i < 2; i++) {
-				if (gameState->Enemies[i].health >= 0) {
-					enemiesLeft++;
-				}
-				else {
-					//Despawn Enemies ***
-					gameState->physics_Controller->setPosition(gameState->Enemies[i].physicsIndex, glm::vec3{ 10000, 10000, 10000 });
-					gameState->Enemies.erase(gameState->Enemies.begin() + (i - 1));
-				}
-			}
-			gameState->enemiesLeft = enemiesLeft;
-
-			//If all Enemies are dead start wave 2
+			enemiesLeft = checkEnemyHealth(gameState);
 			if (enemiesLeft == 0) {
+				spawnPowerUps(gameState);
 				gameState->wave = 2;
-
-				//Spawn powerups
-				gameState->SpawnDynamicObject(1, 25, 0, -25);
 			}
 		}
 
 		//BREAK 1
 		else if (waveBreak == 1) {
-
 			gameState->breakSeconds = breakTime / 60;
 			if (breakTime <= 0) {
 				waveBreak = 2;
 				breakTime = 1800;
-
-				//spawn 2 enemy AIs
-				modeSelection(gameState, gameState->wave);
-				//gameState->SpawnEnemy(0, 35, 5, 35);
-				//gameState->Enemies[1].health = 75;
-				//gameState->SpawnEnemy(0, -35, 5, 35);
-				//gameState->Enemies[2].health = 75;
+				modeSelection(gameState);
 			}
 			breakTime--;
 		}
@@ -110,77 +77,37 @@ void Logic::Update(Gamestate *gameState)
 
 		//WAVE 2
 		else if (gameState->wave == 2) {
-
-			//Check Enemy health
-			enemiesLeft = 0;
-			for (int i = 1; i < 4; i++) {
-				if (gameState->Enemies[i].health >= 0) {
-					enemiesLeft++;
-				}
-				else {
-					//Despawn Enemies ***
-					gameState->physics_Controller->setPosition(gameState->Enemies[i].physicsIndex, glm::vec3{ 10000, 10000, 10000 });
-					gameState->Enemies.erase(gameState->Enemies.begin() + (i - 1));
-				}
-			}
-			gameState->enemiesLeft = enemiesLeft;
-
+			enemiesLeft = checkEnemyHealth(gameState);
 			if (enemiesLeft == 0) {
+				spawnPowerUps(gameState);
 				gameState->wave = 3;
-
-				//spawn power ups
-				gameState->SpawnDynamicObject(1, 15, 0, -15);
-				gameState->SpawnDynamicObject(1, -15, 0, -15);
 			}
 		}
 
 		//BREAK 2
 		else if (waveBreak == 2) {
-
 			gameState->breakSeconds = breakTime / 60;
 			if (breakTime <= 0) {
 				waveBreak = 3;
-				breakTime = 30 * 60;
-
-				//spawn 3 enemy AIs
-				modeSelection(gameState, gameState->wave);
-				//gameState->SpawnEnemy(0, 35, 5, 35);
-				//gameState->Enemies[1].health = 75;
-				//gameState->SpawnEnemy(0, -35, 5, 35);
-				//gameState->Enemies[2].health = 75;
-				//gameState->SpawnEnemy(0, -35, 5, -35);
-				//gameState->Enemies[3].health = 75;
+				breakTime = 1800;
+				modeSelection(gameState);
 			}
 			breakTime--;
 		}
 		
+
 		//WAVE 3
 		else if (gameState->wave == 3) {
-
-		//Check Enemy health
-			enemiesLeft = 0;
-			for (int i = 1; i < 6; i++) {
-				if (gameState->Enemies[i].health >= 0) {
-					enemiesLeft++;
-				}
-				else {
-
-					//Despawn Enemies***
-					gameState->physics_Controller->setPosition(gameState->Enemies[i].physicsIndex, glm::vec3{ 10000, 10000, 10000 });
-					gameState->Enemies.erase(gameState->Enemies.begin() + (i - 1));
-				}
-			}
-			gameState->enemiesLeft = enemiesLeft;
-
+			enemiesLeft = checkEnemyHealth(gameState);
 			if (enemiesLeft == 0) {
+				spawnPowerUps(gameState);
 				gameState->wave = -1; //End here for now
-
-				//spawn power ups
 			}
 		}
+		//BREAK 2
 		else if (waveBreak == 3) {
 			if (breakTime <= 0) {
-				waveBreak = -1;
+				waveBreak = -1; //End here for now
 			}
 			breakTime--;
 		}
@@ -194,56 +121,94 @@ void Logic::Update(Gamestate *gameState)
 	}
 }
 
-void Logic::modeSelection(Gamestate *gameState, int wave) {
-
-	//mode selection
-	srand(time(NULL));
-	int mode = rand() % 3 + 1; //3 modes
-	mode = 1; // reset to mode 1 for now
-
-	switch (mode) {
-	case 1:
-		mode1(gameState, wave);
-		break;
-	case 2:
-		mode2(gameState, wave);
-		break;
-	case 3:
-		mode3(gameState, wave);
-		break;
+int Logic::checkEnemyHealth(Gamestate *gameState) {
+	int enemiesLeft = 0;
+	int end = gameState->Enemies.size()-1;
+	for (int i = 1; i <= gameState->wave; i++) {
+		if (gameState->Enemies[end].health >= 0) {
+			enemiesLeft++;
+		}
+		else {
+			//Despawn Enemies *** this needs the proper despawn function ***
+			gameState->physics_Controller->setPosition(gameState->Enemies[end].physicsIndex, glm::vec3{ 10000 + (i * 10), 10000 + (i * 10), 10000 + (i * 10) });
+		}
+		end--;
 	}
+	gameState->enemiesLeft = enemiesLeft;
+	return enemiesLeft;
 }
 
-//Default mode //Spawn as many enemies as the wave number
-void Logic::mode1(Gamestate *gameState, int wave) {
-	for (int i = 0; i < wave; i++) {
+void Logic::spawnPowerUps(Gamestate *gameState) {
 
+	for (int i = 0; i < gameState->wave; i++) {
 		int mode = i % 4;
 		switch (mode) {
 		case 0: //quadrant 1
-			gameState->SpawnEnemy(0, 35 + (i * 10), 5, 35 + (i * 10));
-			gameState->Enemies[i+1].health = 75;
+			gameState->SpawnDynamicObject(1, 25 + (i * 10), 0, 25 + (i * 10));
 			break;
 		case 1: //quadrant 2
-			gameState->SpawnEnemy(0, -35 - (i * 10), 5, 35 + (i * 10));
-			gameState->Enemies[i+1].health = 75;
+			gameState->SpawnDynamicObject(1, -25 - (i * 10), 1, 25 + (i * 10));
 			break;
 		case 2: //quadrant 3
-			gameState->SpawnEnemy(0, 35 + (i * 10), 5, -35 - (i * 10));
-			gameState->Enemies[i+1].health = 75;
+			gameState->SpawnDynamicObject(1, 25 + (i * 10), 1, -25 - (i * 10));
 			break;
 		case 3: //quadrant 4
-			gameState->SpawnEnemy(0, -35 - (i * 10), 5, -35 - (i * 10));
-			gameState->Enemies[i+1].health = 75;
+			gameState->SpawnDynamicObject(1, -25 - (i * 10), 1, -25 - (i * 10));
 			break;
 		}
 	}
 }
 
-void Logic::mode2(Gamestate *gameState, int wave) {
+void Logic::modeSelection(Gamestate *gameState) {
+
+	//Random mode selection
+	srand(time(NULL));
+	int mode = rand() % 3 + 1; //3 modes for now
+	mode = 1; // reset to mode 1 for now
+
+	switch (mode) {
+	case 1:
+		mode1(gameState);
+		break;
+	case 2:
+		mode2(gameState);
+		break;
+	case 3:
+		mode3(gameState);
+		break;
+	}
+}
+
+//Default mode 
+//Spawn as many enemies as the wave number
+void Logic::mode1(Gamestate *gameState) {
+	for (int i = 0; i < gameState->wave; i++) {
+		int mode = i % 4;
+		switch (mode) {
+		case 0: //quadrant 1
+			gameState->SpawnEnemy(0, 35 + (i * 10), 5, 35 + (i * 10));
+			gameState->Enemies[i + 1].health = 1; //50;
+			break;
+		case 1: //quadrant 2
+			gameState->SpawnEnemy(0, -35 - (i * 10), 5, 35 + (i * 10));
+			gameState->Enemies[i + 1].health = 1; //50;
+			break;
+		case 2: //quadrant 3
+			gameState->SpawnEnemy(0, 35 + (i * 10), 5, -35 - (i * 10));
+			gameState->Enemies[i + 1].health = 1; //50;
+			break;
+		case 3: //quadrant 4
+			gameState->SpawnEnemy(0, -35 - (i * 10), 5, -35 - (i * 10));
+			gameState->Enemies[i + 1].health = 1; //50;
+			break;
+		}
+	}
+}
+
+void Logic::mode2(Gamestate *gameState) {
 
 }
 
-void Logic::mode3(Gamestate *gameState, int wave) {
+void Logic::mode3(Gamestate *gameState) {
 
 }
--- Wasteland_Warrior/Wasteland_Warrior/Logic.h
+++ Wasteland_Warrior/Wasteland_Warrior/Logic.h
@@ -3,17 +3,21 @@
 class Logic
 {
 private:
-	int enemiesLeft;
 	int waveBreak;
 	int breakTime;
+	int enemiesLeft;
+
 public:
 	Logic();
 	~Logic();
 
 	void Update(Gamestate* gameState);
-	void modeSelection(Gamestate *gameState, int wave);
-	void mode1(Gamestate *gameState, int wave);
-	void mode2(Gamestate *gameState, int wave);
-	void mode3(Gamestate *gameState, int wave);
+	int checkEnemyHealth(Gamestate *gameState);
+	void spawnPowerUps(Gamestate *gameState);
+	void modeSelection(Gamestate *gameState);
+	void mode1(Gamestate *gameState);
+	void mode2(Gamestate *gameState);
+	void mode3(Gamestate *gameState);
+
 };
 

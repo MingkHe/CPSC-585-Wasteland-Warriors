b1bea3cf105bd97095b65769d005fdb6e40b14a2
MegaFooby <32686255+MegaFooby@users.noreply.github.com>
2019-03-13 13:46:52 -0600

    Add files via upload

--- Wasteland_Warrior/Wasteland_Warrior/PlayerUnit.cpp
+++ Wasteland_Warrior/Wasteland_Warrior/PlayerUnit.cpp
@@ -9,6 +9,8 @@ PlayerUnit::PlayerUnit(int newPhysicsIndex, int newSceneObjectIndex)
 	physicsIndex = newPhysicsIndex;
 	sceneObjectIndex = newSceneObjectIndex;
 	health = 200;
+
+	heading = glm::vec2(-1, 0);
 }
 
 

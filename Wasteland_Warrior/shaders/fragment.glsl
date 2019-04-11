85b97b00f5989f35d262084600f360ef973d19f9
MegaFooby <cameron.davies2@ucalgary.ca>
2019-04-11 14:31:35 -0600

    Turned shadows down to 20 samples

--- Wasteland_Warrior/shaders/fragment.glsl
+++ Wasteland_Warrior/shaders/fragment.glsl
@@ -6,7 +6,7 @@
 // ==========================================================================
 #version 410
 
-#define SAMPLE_NUM 100
+#define SAMPLE_NUM 20
 #define SAMPLE_RAD 4.0
 #define MID_SAMPLE_RAD 3.5
 #define FAR_SAMPLE_RAD 3.0
@@ -43,7 +43,7 @@ in vec4 shadowCoordthree;
 
 layout(location = 0) out vec4 finalColor;
 
-uniform vec2 poissonDisk[SAMPLE_NUM] = 
+uniform vec2 poissonDisk[100] = 
 { 
 vec2( 0.460925730073f, 0.701913292718f ), 
 vec2( 0.929008298005f, 0.20026975351f ), 
